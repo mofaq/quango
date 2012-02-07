a332b709d4c54e889c0b0d2941eaa2037580d323
Simon Probert <simon.probert1@googlemail.com>
2012-02-07 17:24:32 +1100

    scratch

--- public/javascripts/picup/scratchpad.js
+++ public/javascripts/picup/scratchpad.js
@@ -4,7 +4,7 @@ var currentParams = {}
 //Picup.convertFileInput($('file_upload_input'), currentParams);	
 
 
-function viewScratchURL(currentParams){
-    alert(currentParams);
-    window.open(Picup.urlForOptions('view', currentParams), Picup.windowname);
+function viewScratchURL(callbackURL){
+    alert(callbackURL);
+    window.open(Picup.urlForOptions('new', {'callbackURL':escape(callbackURL)}), Picup.windowname);
 }
