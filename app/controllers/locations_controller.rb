6c41dbd245233c2451c0689de09e3e4072270546
Simon Probert <simon.probert1@googlemail.com>
2012-04-01 15:19:35 +1000

    Loc fixes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -85,7 +85,7 @@ class LocationsController < ApplicationController
   def update
 
     @location = Location.find(params[:id])
-    @location.safe_update(%w[name loc_address_i loc_address_ii loc_city loc_phone loc_state loc_region longitude latitude], params[:location])
+    @location.safe_update(%w[name loc_address_i loc_address_ii loc_city loc_phone loc_postcode loc_state loc_region longitude latitude], params[:location])
 
     @address = [@location.loc_address_i,@location.loc_city].join(',')
     @geo_location = GoogleMapsGeocoder.new(@address)
