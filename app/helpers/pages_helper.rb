834a7b6e43db5af00221b274702843cdf8db61b7
Simon Probert <simon.probert1@googlemail.com>
2012-09-26 11:57:56 +1000

    Scrape tweak

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -12,11 +12,17 @@ module PagesHelper
       scrappy_scrapings = Nokogiri::HTML.parse(scrapings)
       scrappy_scrapings.css('a').each do |link|
     
-      relative_link = link['href']
-
-      link.set_attribute('href', page.scrape_domain + relative_link)
-
+      href_link = link['href']
+      link.set_attribute('href', page.scrape_domain + href_link)
       end
+
+      scrappy_scrapings.css('img').each do |src|
+    
+      src_link = src['src']
+      src.set_attribute('src', page.scrape_domain + src_link)
+      end
+
+
       
       scrapings = scrappy_scrapings
 
